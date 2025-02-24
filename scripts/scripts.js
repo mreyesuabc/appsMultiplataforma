24081fa085e34ebbf227d2b6beccee88e1d93a0a
mreyesuabc <micaela.reyes@uabc.edu.mx>
2025-02-23 16:04:01 -0800

    Reestructuracion de archivos y entregable de tarea paractica1

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+var firdName = "Micaela";
+var lastName = "Reyes";
+// math operations + - 
+let num1 = 10;
+let num2 = 12;
+
+console.log("El resultado es = " + (num1 + num2));
+console.log("El resultado es = " + (num1 - num2));
+console.log("El resultado es = " + (num1 * num2));
+console.log("El resultado es = " + (num1 / num2).toFixed(3));
+alert("este es un mensaje de alert");
